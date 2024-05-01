8917b4c456f0bf598441b176c397aaa764c310ba
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2024-05-01 13:08:11 -0300

    feat: add font config to tailwind

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -2,10 +2,30 @@ import type { Config } from "tailwindcss";
 
 const config: Omit<Config, "content"> = {
   theme: {
+    fontSize: {
+      xxs: ["0.625rem", "1.25rem"],
+      xs: ["0.75rem", "1.5rem"],
+      sm: ["0.875rem", "1.5rem"],
+      base: ["1rem", "1.5rem"],
+      lg: ["1.125rem", "1.5rem"],
+      xl: ["1.25rem", "1.5rem"],
+      "2xl": ["1.5rem", "2rem"],
+      "3xl": ["2rem", "2rem"],
+      "4xl": ["2.5rem", "3rem"],
+      "5xl": ["3rem", "3rem"],
+      "6xl": ["4rem", "4rem"]
+    },
+    lineHeight: {
+      none: 1,
+      tight: 1.25,
+      short: 1.4,
+      base: 1.6
+    },
     extend: {
-      backgroundImage: {
-        "glow-conic":
-          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
+      fontFamily: {
+        brand: ['Merriweather', 'serif'],
+        sans: ["Inter", "sans-serif"],
+        mono: ["JetBrains Mono", "monospace"],
       },
     },
   },
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,5 +1,7 @@
 export function Button(): JSX.Element {
   return (
-    <button className="font-medium bg-[#BEF264] color-[#1A2E05] px-4 py-2 rounded">Hello Nivo</button>
+    <button className="font-medium bg-[#BEF264] color-[#1A2E05] px-4 py-2 rounded" type="button">
+      Hello Nivo
+    </button>
   );
 }
