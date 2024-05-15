511e6f21d7b796d7c5416a6d913dff7de186b427
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2024-05-15 09:04:03 -0300

    feat: add initial web project

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@nivovideo/ui/button'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Button variant="primary">Click me</Button>
+    </div>
+  )
+}
